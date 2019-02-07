5b20b551280eae55679b96d41503be4431b4846d
IvanovEvgeniy <yakudZZZa@list.ru>
2019-02-07 21:39:02 +0500

    Initial Commit

---
+++ VKapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VKapp
+//
+//  Created by Евгений Иванов on 07/02/2019.
+//  Copyright © 2019 Евгений Иванов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
